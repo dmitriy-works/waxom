dd69c531f762821909568cd366de9cea914981d7
dmitriy-works <dmitriy-works@ukr.net>
2021-07-31 11:37:03 +0300

    section latest

--- app/js/script.js
+++ app/js/script.js
@@ -10,79 +10,58 @@ $(function(){
     $('.header__menu-list').slideToggle();
   });
 
-  $(".header__slider").slick({
-    arrows: true,
-    dots:true,    
-  });
-
-  $(".header__slider").slick({       
-    slidesToShow: 4,
+  $('.header__slider').slick({ 
+    dots:true,      
+    slidesToShow: 1,
     slidesToScroll: 1,
-    // prevArrow:'<button class="slick-arrow slick-prev"><img src="images/prev.png" alt=""></button>',
-    // nextArrow:'<button class="slick-arrow slick-next"><img src="images/next.png" alt=""></button>',
-
-    responsive: [
-    {
-      breakpoint: 911,
-      settings: {
-        slidesToShow: 3,        
-        infinite: true,        
-      }
-    },
-
-    {
-      breakpoint: 681,
-      settings: {
-        slidesToShow: 2,        
-        infinite: true,        
-      }
-    },
-
-    {
-      breakpoint: 445,
-      settings: {
-        slidesToShow: 1,        
-        infinite: true,        
-      }
-    },
-        
-  ]
-  });
-
-  $('.header__menu-btn').on('click', function(){
-    $('.header__menu > ul').slideToggle();
-  });
-
-   $('.header__drop-down.drop-down').on('click', function(){
-    $(this).children('.drop-down__list').toggleClass('active');
-    $(this).children('.drop-down__link').toggleClass('active');
+    prevArrow:'<button class="slick__header-arrow header-prev"><img src="images/header-prev.png" alt="header-prev"></button>',
+    nextArrow:'<button class="slick__header-arrow header-next"><img src="images/header-next.png" alt="header-next"></button>',
+    fade:true,
+    
    
   });
 
-  $(".js-range-slider").ionRangeSlider({
-        type: "double",       
-        min: 0,
-        max: 1000,
-        from: 0,
-        to: 600,
-        prefix: "$"
-    });
-
-    $('.icon-th-list').on('click', function(){
-      $('.products__item').addClass('list');
-      $('.icon-th-large').removeClass('active');
-      $('.icon-th-list').addClass('active');
-    });
-
-    $('.icon-th-large').on('click', function(){
-      $('.products__item').removeClass('list');
-       $('.icon-th-list').removeClass('active');
-       $('.icon-th-large').addClass('active');
-      
-      
-    });
-
+  $(".waxom-slider__inner").slick({    
+    slidesToShow: 4,
+    slidesToScroll: 2,    
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="images/prev.svg" alt="prev-arrow"></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><img src="images/next.svg" alt="next-arrow"></button>',
+    responsive: [
+    {
+      breakpoint: 1198,
+      settings: {
+        
+        slidesToShow: 4,
+      }
+    },
+    {
+      breakpoint: 985,
+      settings: {        
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 700,
+      settings: {        
+        slidesToShow: 2,        
+      }
+    },
+    {
+      breakpoint: 451,
+      settings: {        
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 371,
+      settings: {        
+        slidesToShow: 1,
+      }
+    },
+  ]
     
+  });
 
-  var mixer = mixitup('.products__inner-box');
+
+  
 });
